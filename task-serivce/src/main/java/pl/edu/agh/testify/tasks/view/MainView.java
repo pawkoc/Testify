4ba0e39298cf71914bd0016703758a0079261c73
Nequeq <nequeq@gmail.com>
2017-06-08 00:26:44 +0200

    fix for upload

---
+++ task-serivce/src/main/java/pl/edu/agh/testify/tasks/view/MainView.java
@@ -0,0 +1,73 @@
+package pl.edu.agh.testify.tasks.view;
+
+import com.vaadin.annotations.VaadinServletConfiguration;
+import com.vaadin.server.VaadinRequest;
+import com.vaadin.server.VaadinServlet;
+import com.vaadin.spring.annotation.SpringUI;
+import com.vaadin.ui.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.RestTemplate;
+import pl.edu.agh.testify.tasks.model.Task;
+import pl.edu.agh.testify.tasks.repository.TaskRepository;
+
+import javax.servlet.annotation.WebServlet;
+
+@SpringUI
+public class MainView extends UI {
+
+    TaskRepository repo;
+    Grid<Task> grid;
+    TextField filename;
+    TextField taskName;
+    TextField summary;
+    TextField taskData;
+
+    @Autowired
+    public MainView(TaskRepository repo) {
+        this.repo = repo;
+        this.grid = new Grid<>(Task.class);
+        this.filename = new TextField();
+        this.taskName = new TextField();
+        this.summary = new TextField();
+        this.taskData = new TextField();
+    }
+
+    @Override
+    protected void init(VaadinRequest request) {
+        final VerticalLayout layout = new VerticalLayout();
+        layout.addComponent(new Label("Here is list of found tasks:"));
+        listTasks();
+        layout.addComponents(grid);
+        filename.setPlaceholder("Filename");
+        taskName.setPlaceholder("Name for task");
+        summary.setPlaceholder("Summary");
+        taskData.setPlaceholder("Data");
+        layout.addComponents(filename);
+        layout.addComponents(taskName);
+        layout.addComponents(summary);
+        layout.addComponents(taskData);
+
+        RestTemplate restTemplate = new RestTemplate();
+
+        Button submit = new Button("Upload", event -> {
+            MultiValueMap<String, String> mvm = new LinkedMultiValueMap<String, String>();
+            mvm.add("file", taskData.getValue());
+            mvm.add("taskName", taskName.getValue());
+            mvm.add("summary", summary.getValue());
+            restTemplate.postForObject("http://localhost:11226/task/upload", mvm, MultiValueMap.class);
+        });
+        layout.addComponents(submit);
+        setContent(layout);
+    }
+
+    private void listTasks() {
+        grid.setItems(repo.findAll());
+    }
+
+    @WebServlet(value = "/*", asyncSupported = true)
+    @VaadinServletConfiguration(productionMode = false, ui = MainView.class)
+    public static class Servlet extends VaadinServlet {
+    }
+}
