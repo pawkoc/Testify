0aefae951de72057aa68e618b93b29415dca4f6f
Paweł Kocot <pawel.kacper.kocot@gmail.com>
2017-06-11 01:46:49 +0200

    Client vaadin init

---
+++ client/src/main/java/pl/edu/agh/testify/client/ClientApplication.java
@@ -0,0 +1,12 @@
+package pl.edu.agh.testify.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClientApplication.class, args);
+    }
+}
