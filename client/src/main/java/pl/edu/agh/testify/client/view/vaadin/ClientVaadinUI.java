63f31c49bbd4c928a94ba7213dd1e5231526fce0
Paweł Kocot <pawel.kacper.kocot@gmail.com>
2017-06-11 01:46:49 +0200

    Client vaadin init

---
+++ client/src/main/java/pl/edu/agh/testify/client/view/vaadin/ClientVaadinUI.java
@@ -0,0 +1,14 @@
+package pl.edu.agh.testify.client.view.vaadin;
+
+import com.vaadin.server.VaadinRequest;
+import com.vaadin.spring.annotation.SpringUI;
+import com.vaadin.ui.UI;
+
+@SpringUI
+public class ClientVaadinUI extends UI {
+
+    @Override
+    protected void init(VaadinRequest vaadinRequest) {
+
+    }
+}
