120b62f03d5130f878e226ed27ef0f827f5eb20b
Paweł Kocot <pawel.kacper.kocot@gmail.com>
2017-06-11 01:32:03 +0200

    Get rid of unnecessary services

--- commons/src/main/java/pl/edu/agh/testify/commons/composite/CompositeTask.java
+++
@@ -1,6 +0,0 @@
-package pl.edu.agh.testify.commons.composite;
-
-public class CompositeTask {
-
-//    private
-}
--- task-summary/src/main/java/pl/edu/agh/testify/model/SummaryRepository.java
+++
@@ -1,7 +0,0 @@
-package pl.edu.agh.testify.model;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface SummaryRepository extends MongoRepository<Summary, String> {
-
-}
