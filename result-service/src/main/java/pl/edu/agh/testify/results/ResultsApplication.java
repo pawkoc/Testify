0d6b7c449ce9dd104b85f48cf70bd6e1fd21a74c
Paweł Kocot <pawel.kacper.kocot@gmail.com>
2017-06-22 22:00:46 +0200

    result-service init

---
+++ result-service/src/main/java/pl/edu/agh/testify/results/ResultsApplication.java
@@ -0,0 +1,12 @@
+package pl.edu.agh.testify.results;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResultsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ResultsApplication.class, args);
+    }
+}
